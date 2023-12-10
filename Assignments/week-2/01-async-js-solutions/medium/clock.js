54f418a15bc398dc11ab5838778f29e3b8c4edb2
mohits-git <mohitkhatri2805@gmail.com>
2023-12-10 12:17:21 +0530

    Refactored clock.js

--- Assignments/week-2/01-async-js-solutions/medium/clock.js
+++ Assignments/week-2/01-async-js-solutions/medium/clock.js
@@ -1,33 +1,48 @@
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
+function displayTime() {
+    setTimeout(() => {
+        let d = new Date();
+        let meridian = d.getHours() >= 12 ? "PM" : "AM";
+        let hour = d.getHours() === 12 ? d.getHours() : d.getHours()%12;
+        console.clear();
+        console.log(hour + ":" + d.getMinutes() + ":" + d.getSeconds() + " " + meridian);
+        displayTime();
+    }, 1000)
+}
 
-let meridian = "PM";
+displayTime();
 
-let currDate = new Date();
+//Old approach...
 
-meridian = currDate.getHours() >= 12 ? "PM": "AM";
-hours = currDate.getHours()%12;
-minutes = currDate.getMinutes();
-seconds = currDate.getSeconds();
-
-setInterval(()=>{
-    seconds++;
-    if(seconds === 60){
-        seconds = 0;
-        minutes++;
-        if(minutes=== 60){
-            minutes = 0;
-            hours++;
-            if(hours === 12){
-                pm = (pm === "PM")?"AM": "PM";
-            }
-            if(hours === 13){
-                hours = 1;
-            }
-        }
-    }
-    console.clear();
-    console.log(hours + ":" + minutes + ":" + seconds + " " + meridian);
-}, 1000)
-
+//let hours = 0;
+//let minutes = 0;
+//let seconds = 0;
+//
+//let meridian = "PM";
+//
+//let currDate = new Date();
+//
+//meridian = currDate.getHours() >= 12 ? "PM": "AM";
+//hours = currDate.getHours()%12;
+//minutes = currDate.getMinutes();
+//seconds = currDate.getSeconds();
+//
+//setInterval(()=>{
+//    seconds++;
+//    if(seconds === 60){
+//        seconds = 0;
+//        minutes++;
+//        if(minutes=== 60){
+//            minutes = 0;
+//            hours++;
+//            if(hours === 12){
+//                pm = (pm === "PM")?"AM": "PM";
+//            }
+//            if(hours === 13){
+//                hours = 1;
+//            }
+//        }
+//    }
+//    console.clear();
+//    console.log(hours + ":" + minutes + ":" + seconds + " " + meridian);
+//}, 1000)
+//
