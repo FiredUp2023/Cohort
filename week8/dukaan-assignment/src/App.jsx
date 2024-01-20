f0912ba9b360ddffc7fcb159f98621a87de6e1bc
mohits-git <mohitkhatri2805@gmail.com>
2024-01-20 23:30:04 +0530

    Cohort week 8

---
+++ week8/dukaan-assignment/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-between'>
+      <div className='bg-red-400'>hi1</div>
+      <div className='bg-blue-400'>hi2</div>
+      <div className='bg-yellow-400'>hi3</div>
+      </div>
+    </>
+  )
+}
+
+export default App
