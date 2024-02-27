f0527c18c0f40f4fca3670a97aa3767669013bad
mohits-git <mohitkhatri2805@gmail.com>
2024-02-27 21:48:13 +0530

    Learning Connection Pooling for Deploying on Cloudflare workers

---
+++ week12/gentle-wood-0c7f/README.md
@@ -0,0 +1,2 @@
+## don't forget to put your prisma.accelerate pooled connection url (DATABASE_URL) in wrangler.toml and .env
+## and your database DIRECT_URL in .env
---
+++ week12/gentle-wood-0c7f/src/index.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+export interface Env {
+	DATABASE_URL: string
+}
+
+export default {
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext
+	): Promise<Response> {
+
+		try {
+			const prisma = new PrismaClient({
+				datasourceUrl: env.DATABASE_URL,
+			}).$extends(withAccelerate())
+
+			const response = await prisma.log.create({
+				data: {
+					level: 'Info',
+					message: `${request.method} ${request.url}`,
+					meta: {
+						headers: JSON.stringify(request.headers),
+					},
+				},
+			})
+
+			//    const { data, info } = await prisma.log
+			//      .findMany({
+			//        take: 20,
+			//        orderBy: {
+			//          id: 'desc',
+			//        },
+			//      })
+			//      .withAccelerateInfo()
+
+			//    console.log(JSON.stringify(info))
+
+			return Response.json(response)
+		}
+		catch (error: any) {
+			return Response.json({ message: "Something went wrong", error: error.message }, { status: 500 });
+		}
+	},
+}
